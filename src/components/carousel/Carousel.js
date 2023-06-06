d5c4d831704e04388016bb093f6c38e7213c38cf
Brandon <ihartappraisals@gmail.com>
2023-06-05 23:23:18 -0700

    responsive design fixed for loader

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import hint from "../../images/carousel-hint.png";
 // import function to register Swiper custom elements
 import { register } from "swiper/element/bundle";
 import { isMobile } from "mobile-device-detect";
 import { appData } from "../../data/appData";
+import { useEffect } from "react";
 
 // register Swiper custom elements
 register();
+
 const Carousel = () => {
   const { images } = appData;
+  useEffect(() => {
+    const swiperEl = document.querySelector("swiper-container");
 
+    // swiper parameters
+    const swiperParams = {
+      slidesPerView: isMobile ? 1 : 3,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+      },
+      on: {
+        init() {
+          // ...
+        },
+      },
+    };
+
+    // now we need to assign all parameters to Swiper element
+    Object.assign(swiperEl, swiperParams);
+
+    // and now initialize it
+    swiperEl.initialize();
+  }, []);
   const generateSlides = () =>
     images.map((img, i) => {
       return (
@@ -25,8 +56,6 @@ const Carousel = () => {
       </div>
       <swiper-container
         effect={isMobile ? "cube" : "slide"}
-        slides-per-view={isMobile ? "1" : "3"}
-        slides-per-group={1}
         centeredSlides="true"
         rewind="true"
         autoplay="true"
