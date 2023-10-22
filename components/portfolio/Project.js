9adca9ad9db946608a4883efe8a23cee11a263f1
Brandon <ihartappraisals@gmail.com>
2023-10-21 23:19:59 -0700

    set up loader

--- components/portfolio/Project.js
+++ components/portfolio/Project.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Project = ({ item, i, setImgsLoaded, count }) => {
   return (
     <>
-      <div className=" w-11/12 projects-wrapper m-auto flex justify-center items-center flex-col md:[&:nth-child(even)&_article]:flex-row-reverse md:[&:nth-child(even)&_figure]:mr-0 md:[&:nth-child(even)&_figure]:ml-5 ">
+      <div className=" w-11/12 projects-wrapper m-auto flex justify-center items-center flex-col  text-black md:[&:nth-child(even)&_article]:flex-row-reverse md:[&:nth-child(even)&_figure]:mr-0 md:[&:nth-child(even)&_figure]:ml-5 ">
         {i === 0 && <h1 className="mb-10 text-center md:mb-12">Portfolio</h1>}{" "}
         <a href={item.url} target="_blank">
           <article className="w-full flex mb-24 flex-col items-center m-auto md:flex-row md:items-start md:w-fit lg:mb-40">
