c45286e270a70f13602140fe242cf233118abffb
Brandon <ihartappraisals@gmail.com>
2023-10-28 22:54:04 -0700

    fixed landing height

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,8 @@ export default function Home() {
   const { count, imgsLoaded, setImgsLoaded } = createRef();
 
   return (
-    <section className="relative flex flex-col items-center mt-12 md:h-screen md:mt-0 md:justify-center lg:mt-0 lg:justify-center">
-
+    <section className="landing relative flex flex-col items-center justify-center md:h-screen md:mt-0 md:justify-center lg:mt-0 lg:justify-center">
       <LandingHero />
- 
     </section>
   );
 }
