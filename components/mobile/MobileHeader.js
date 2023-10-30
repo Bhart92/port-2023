213bd009c54acf62c56011b8b47fd8b2cfa17a93
Brandon Hart <ihartappraisals@gmail.com>
2023-10-30 15:31:31 -0700

    img opt

--- components/mobile/MobileHeader.js
+++ components/mobile/MobileHeader.js
@@ -2,11 +2,13 @@ import Logo from "../header/Logo.js";
 import MobileNav from "../mobile/MobileNav.js";
 import Link from "next/link";
 import { useState } from "react";
-import MobileNavLinks from "./MobileNavLinks";
+import dynamic from "next/dynamic";
 
 const MobileHeader = () => {
   const [isOpen, setOpen] = useState(false);
-
+  const MobileNavLinks = dynamic(() => import("./MobileNavLinks"), {
+    ssr: false,
+  });
   return (
     <div className=" relative px-6 h-20 flex justify-between items-center bg-neutral-900 text-white lg:h-full lg:min-h-screen lg:max-h-screen">
       <div className="bg-white-900 absolute top-0 bottom-0 left-0 right-0 w-full h-full z-20" />
